c18b6e4689a9190d82b71f5a624809121759aef8
Inggage <prahlad@inggage.com>
2024-07-20 17:12:11 +0530

    react-google-analytics

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello, this is my react application and it is home page.
+    </div>
+  );
+}
+
+export default App;
